2157d81745a56018c53ccf3b15420b6d4cf6bf01
sebpalluel <sebpalluel@gmail.com>
2023-02-21 11:46:53 +0100

    implemented signature example outside of scw scope

--- nextjs-biconomy-web3Auth-nextAuth-siwe/pages/index.tsx
+++ nextjs-biconomy-web3Auth-nextAuth-siwe/pages/index.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 const Index = () => {
   const SocialLoginDynamic = dynamic(
@@ -9,11 +9,35 @@ const Index = () => {
     }
   );
 
+  const createSignature = async () => {
+    const signature = await window.biconomySmartAccount
+      ?.getsigner()
+      ?.signMessage("Hello World");
+    console.log({ signature });
+    setSignature(signature);
+    return signature;
+  };
+  const [signature, setSignature] = useState<string | undefined>(undefined);
+
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
         <SocialLoginDynamic />
       </Suspense>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ margin: "auto" }}>
+          <button onClick={createSignature}>Sign message Hello World</button>
+        </div>
+        <div style={{ margin: "auto" }}>
+          {signature && <div>Signature: {signature}</div>}
+        </div>
+      </div>
     </div>
   );
 };
