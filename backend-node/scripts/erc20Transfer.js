4f7dcee24ceba8efd05f69a2cb8d64f146e68bce
amanraj1608 <archanaamanraj@gmail.com>
2023-03-24 13:37:20 +0530

    tx hash and userOp hash response

--- backend-node/scripts/erc20Transfer.js
+++ backend-node/scripts/erc20Transfer.js
@@ -1,7 +1,7 @@
 const { ethers } = require("ethers");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const SmartAccount = require("@biconomy-sdk-dev/smart-account").default;
-const { ChainId } = require("@biconomy-sdk-dev/core-types");
+const { ChainId, Environments } = require("@biconomy-sdk-dev/core-types");
 const config = require("../config.json");
 
 const erc20Transfer = async (recipientAddress, amount, tokenAddress) => {
@@ -9,14 +9,9 @@ const erc20Transfer = async (recipientAddress, amount, tokenAddress) => {
   const walletProvider = new ethers.providers.Web3Provider(provider);
   // create SmartAccount instance
   const wallet = new SmartAccount(walletProvider, {
-    debug: false,
+    environment: Environments.QA,
     activeNetworkId: config.chainId,
-    backendUrl: 'https://sdk-backend.test.biconomy.io/v1',
-    socketServerUrl: 'wss://sdk-testing-ws.staging.biconomy.io/connection/websocket',
-    relayerUrl: 'https://sdk-relayer.staging.biconomy.io/api/v1/relay',
-    bundlerUrl: 'https://sdk-relayer.test.biconomy.io/api/v1/relay',
     supportedNetworksIds: [ChainId.GOERLI, ChainId.POLYGON_MUMBAI],
-    biconomySigningServiceUrl: 'https://paymaster-signing-service.staging.biconomy.io/api/v1/sign',
     networkConfig: [
       {
         chainId: config.chainId,
@@ -53,7 +48,9 @@ const erc20Transfer = async (recipientAddress, amount, tokenAddress) => {
 
   // Sending transaction
   const txResponse = await smartAccount.sendGaslessTransaction({ transaction: tx });
-  console.log('Transaction hash', txResponse.hash);
+  console.log('Tx Response', txResponse);
+  const txReciept = await txResponse.wait();
+  console.log('Tx hash', txReciept.transactionHash);
 }
 
 module.exports = { erc20Transfer };
