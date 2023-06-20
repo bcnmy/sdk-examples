2e4fc8e0c93595db42c1305638bd666f38ade74c
Ankush Swarnakar <ankushswar1@gmail.com>
2023-06-20 11:41:46 -0400

    Testing with embedded wallet provider

--- nextjs-biconomy-web3Auth/components/scw.tsx
+++ nextjs-biconomy-web3Auth/components/scw.tsx
@@ -4,8 +4,11 @@ import { ethers } from "ethers";
 import { ChainId } from "@biconomy/core-types";
 import SocialLogin from "@biconomy/web3-auth";
 import SmartAccount from "@biconomy/smart-account";
+import { usePrivy, useWallets } from "@privy-io/react-auth";
 
 const Home = () => {
+  const {ready, authenticated, login, logout} = usePrivy();
+  const {wallets} = useWallets();
   const [provider, setProvider] = useState<any>();
   const [account, setAccount] = useState<string>();
   const [smartAccount, setSmartAccount] = useState<SmartAccount | null>(null);
@@ -15,93 +18,88 @@ const Home = () => {
     null
   );
 
-  const connectWeb3 = useCallback(async () => {
-    if (typeof window === "undefined") return;
-    console.log("socialLoginSDK", socialLoginSDK);
-    if (socialLoginSDK?.provider) {
-      const web3Provider = new ethers.providers.Web3Provider(
-        socialLoginSDK.provider
-      );
-      setProvider(web3Provider);
-      const accounts = await web3Provider.listAccounts();
-      setAccount(accounts[0]);
+  const onCreateSmartAccount = async () => {
+    const embeddedWallet = wallets.find((wallet) => wallet.walletClientType === 'privy');
+    const provider = await embeddedWallet?.getEthersProvider();
+    if (!provider) {
+      console.error('Unable to retrieve ethers provider from embedded wallet.');
       return;
     }
-    if (socialLoginSDK) {
-      socialLoginSDK.showWallet();
-      return socialLoginSDK;
-    }
-    const sdk = new SocialLogin();
-    await sdk.init({
-      chainId: ethers.utils.hexValue(80001),
-    });
-    setSocialLoginSDK(sdk);
-    sdk.showWallet();
-    return socialLoginSDK;
-  }, [socialLoginSDK]);
+    let options = {
+      activeNetworkId: ChainId.GOERLI,
+      supportedNetworksIds: [ ChainId.GOERLI ]}
 
-  // if wallet already connected close widget
-  useEffect(() => {
-    console.log("hidelwallet");
-    if (socialLoginSDK && socialLoginSDK.provider) {
-      socialLoginSDK.hideWallet();
-    }
-  }, [account, socialLoginSDK]);
+     let smartAccount = new SmartAccount(provider, options);
+     smartAccount = await smartAccount.init();
 
-  // after metamask login -> get provider event
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      if (account) {
-        clearInterval(interval);
-      }
-      if (socialLoginSDK?.provider && !account) {
-        connectWeb3();
-      }
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [account, connectWeb3, socialLoginSDK]);
+  }
+  // // if wallet already connected close widget
+  // useEffect(() => {
+  //   console.log("hidelwallet");
+  //   if (socialLoginSDK && socialLoginSDK.provider) {
+  //     socialLoginSDK.hideWallet();
+  //   }
+  // }, [account, socialLoginSDK]);
 
-  const disconnectWeb3 = async () => {
-    if (!socialLoginSDK || !socialLoginSDK.web3auth) {
-      console.error("Web3Modal not initialized.");
-      return;
-    }
-    await socialLoginSDK.logout();
-    socialLoginSDK.hideWallet();
-    setProvider(undefined);
-    setAccount(undefined);
-    setScwAddress("");
-  };
+  // // after metamask login -> get provider event
+  // useEffect(() => {
+  //   const interval = setInterval(async () => {
+  //     if (account) {
+  //       clearInterval(interval);
+  //     }
+  //     if (socialLoginSDK?.provider && !account) {
+  //       connectWeb3();
+  //     }
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, [account, connectWeb3, socialLoginSDK]);
 
-  useEffect(() => {
-    async function setupSmartAccount() {
-      setScwAddress("");
-      setScwLoading(true);
-      const smartAccount = new SmartAccount(provider, {
-        activeNetworkId: ChainId.GOERLI,
-        supportedNetworksIds: [ChainId.GOERLI],
-      });
-      await smartAccount.init();
-      const context = smartAccount.getSmartAccountContext();
-      setScwAddress(context.baseWallet.getAddress());
-      setSmartAccount(smartAccount);
-      setScwLoading(false);
-    }
-    if (!!provider && !!account) {
-      setupSmartAccount();
-      console.log("Provider...", provider);
-    }
-  }, [account, provider]);
+  // const disconnectWeb3 = async () => {
+  //   if (!socialLoginSDK || !socialLoginSDK.web3auth) {
+  //     console.error("Web3Modal not initialized.");
+  //     return;
+  //   }
+  //   await socialLoginSDK.logout();
+  //   socialLoginSDK.hideWallet();
+  //   setProvider(undefined);
+  //   setAccount(undefined);
+  //   setScwAddress("");
+  // };
+
+  // useEffect(() => {
+  //   async function setupSmartAccount() {
+  //     setScwAddress("");
+  //     setScwLoading(true);
+  //     const smartAccount = new SmartAccount(provider, {
+  //       activeNetworkId: ChainId.GOERLI,
+  //       supportedNetworksIds: [ChainId.GOERLI],
+  //     });
+  //     await smartAccount.init();
+  //     const context = smartAccount.getSmartAccountContext();
+  //     setScwAddress(context.baseWallet.getAddress());
+  //     setSmartAccount(smartAccount);
+  //     setScwLoading(false);
+  //   }
+  //   if (!!provider && !!account) {
+  //     setupSmartAccount();
+  //     console.log("Provider...", provider);
+  //   }
+  // }, [account, provider]);
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>Biconomy SDK Next.js Web3Auth Example</h1>
-        <button onClick={!account ? connectWeb3 : disconnectWeb3}>
-          {!account ? "Connect Wallet" : "Disconnect Wallet"}
+        <button disabled={!ready} onClick={!authenticated ? login : logout}>
+          {!authenticated ? "Login" : "Logout"}
         </button>
+        {(ready && authenticated) && (
+          <button onClick={onCreateSmartAccount}>
+            Create smart account
+          </button>
+        )}
 
         {account && (
           <div>
--- nextjs-biconomy-web3Auth/pages/_app.tsx
+++ nextjs-biconomy-web3Auth/pages/_app.tsx
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "@biconomy/web3-auth/dist/src/style.css"
+import {PrivyProvider} from '@privy-io/react-auth'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <PrivyProvider
+      appId='cla06f34x0001mh08l8nsr496'
+      apiUrl='https://auth.staging.privy.io'
+      config={{
+        embeddedWallets: {
+          createOnLogin: 'all-users'
+        }
+      }}
+    >
+      <Component {...pageProps} />
+    </PrivyProvider>
+  );
 }
