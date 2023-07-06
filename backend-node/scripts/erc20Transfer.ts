89d88361f61f411eccdbe1a764a24dcfc598c9ad
talhamalik883 <talhamalik883@gmail.com>
2023-07-07 03:22:00 +0500

    removed hardcoded decimals

--- backend-node/scripts/erc20Transfer.ts
+++ backend-node/scripts/erc20Transfer.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+const { ERC20ABI } = require('./abi')
 import inquirer from "inquirer";
 import {
   IHybridPaymaster,
@@ -17,16 +18,17 @@ export const erc20Transfer = async (
 ) => {
   const biconomySmartAccount = await createBiconomyAccountInstance();
 
-  // transfer ERC-20 tokens to recipient
-  const erc20Interface = new ethers.utils.Interface([
-    "function transfer(address _to, uint256 _value)",
-  ]);
   // Encode an ERC-20 token transfer to recipient of the specified amount
-  const amountGwei = ethers.utils.parseUnits(amount.toString(), 6); // TODO: if it's USDC on Mumbai and decimals are 6
-  const data = erc20Interface.encodeFunctionData("transfer", [
-    recipientAddress,
-    amountGwei,
-  ]);
+  const readProvider = new ethers.providers.JsonRpcProvider(config.rpcUrl)
+  const tokenContract = new ethers.Contract(tokenAddress, ERC20ABI, readProvider)
+  let decimals = 1
+  try {
+    decimals = await tokenContract.decimals()
+  } catch (error) {
+    throw new Error('invalid token address supplied')
+  }
+  const amountGwei = ethers.utils.parseUnits(amount.toString(), decimals)
+  const data = (await tokenContract.populateTransaction.transfer(recipientAddress, amountGwei)).data
   const transaction = {
     to: tokenAddress,
     data,
